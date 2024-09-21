fc0f7f1e609b9f983c8a1e0c5c1ab4abd2abdfe7
ovodomina <yuri.costa4@fatec.sp.gov.br>
2024-09-21 10:35:18 -0300

    [JS]
    Adiciona funcionalidade para alterar o texto do parágrafo ao clicar no botão.
    
    Co-Authored-By: Valdir de Jesus  <112215070+valdirgarcia@users.noreply.github.com>
    Co-Authored-By: frqnk <33494411+frqnk@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//Arquivo vazio por enquanto
+// Função para alterar o conteúdo do parágrafo quando o botão for clicado
+document.getElementById('meuBotao').addEventListener('click', function() {
+    var paragrafo = document.getElementById('paragrafo');
+    paragrafo.textContent = "Você clicou no botão! O texto foi alterado.";
+    });
